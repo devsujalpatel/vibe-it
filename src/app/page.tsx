b4de326c922fa747166c76b5fbfe0b545c1597b7
Sujal Patel <sujal7455@gmail.com>
2025-06-27 19:08:05 +0530

    prisma reset

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,9 @@
 
-import { prisma } from "@/lib/db";
 
 const Page = async () => {
-  const posts = await prisma.post.findMany();
+
   return <div className="text-3xl">
-    {JSON.stringify(posts, null, 2)}
+    Hello World
   </div>;
 };
 
