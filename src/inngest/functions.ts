324cc830b684da1f801ef234020673aaf4beb445
sujalpatelcoder <sujal7455@gmail.com>
2025-07-24 00:42:50 +0530

    added new ai job and api key

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -4,7 +4,8 @@ export const helloWorld = inngest.createFunction(
   { id: "hello-world" },
   { event: "test/hello.world" },
   async ({ event, step }) => {
-    await step.sleep("wait-a-moment", "30s");
+    await step.sleep("wait-a-moment", "5s");
+
     return { message: `Hello ${event.data.email}!` };
   },
 );
