61667d72a9fdb472568de6369e28c746aa40c192
sujalpatelcoder <sujal7455@gmail.com>
2025-07-26 02:03:11 +0530

    feat:fragment web page created

---
+++ src/components/hind.tsx
@@ -0,0 +1,14 @@
+"use client"
+
+import {
+    Tooltip, 
+    TooltipContent,
+    TooltipProvider,
+    TooltipTrigger
+} from "@/components/ui/tooltip"
+
+export const hint = () => {
+  return (
+    <div>hind</div>
+  )
+}
