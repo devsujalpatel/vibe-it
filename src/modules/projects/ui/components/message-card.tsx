a8f192fc9982bce105cc9db15ff8847dd13d3a3c
sujalpatelcoder <sujal7455@gmail.com>
2025-07-25 16:04:50 +0530

    textarea and button added

--- src/modules/projects/ui/components/message-card.tsx
+++ src/modules/projects/ui/components/message-card.tsx
@@ -1,4 +1,6 @@
 import { format } from "date-fns";
+import { ChevronRightIcon, Code2Icon } from "lucide-react";
+import Image from "next/image";
 
 import { Card } from "@/components/ui/card";
 import { Fragment, MessageType } from "@/generated/prisma";
@@ -15,6 +17,40 @@ const UserMessage = ({ content }: UserMessageProps) => {
         {content}
       </Card>
     </div>
+  );
+};
+
+interface FragmentCardProps {
+  fragment: Fragment;
+  isActiveFragment: boolean;
+  onFragmentClick: (fragment: Fragment) => void;
+}
+
+const FragmentCard = ({
+  fragment,
+  isActiveFragment,
+  onFragmentClick,
+}: FragmentCardProps) => {
+  return (
+    <button
+      className={cn(
+        "flex items-start text-start gap-2 border rounded-lg bg-muted w-fit p-3 hover:bg-secondary transition-colors",
+        isActiveFragment &&
+          "bg-primary text-primary-foreground border-primary hover:bg-primary"
+      )}
+      onClick={() => onFragmentClick(fragment)}
+    >
+      <Code2Icon className="size-4 mt-0.5" />
+      <div className="flex flex-col flex-1">
+        <span className="font-medium text-sm line-clamp-1">
+          {fragment.title}
+        </span>
+        <span className="text-sm">Preview</span>
+      </div>
+      <div className="flex items-center justify-center mt-0.5">
+        <ChevronRightIcon className="size-4" />
+      </div>
+    </button>
   );
 };
 
@@ -43,13 +79,27 @@ const AssistantMessage = ({
       )}
     >
       <div className="flex items-center gap-2 pl-2 mb-2">
-        <span className="text-sm font-medium">VibeIt</span>
+        <Image
+          src="/logo.svg"
+          width={18}
+          height={18}
+          className="shrink-0"
+          alt="Vibe It logo"
+        />
+        <span className="text-sm font-medium">Vibe It</span>
         <span className="text-xsm text-muted-foreground opacity-0 transition-opacity group-hover:opacity-100">
           {format(createdAt, "HH:mm 'on' MMM dd, yyyu")}
         </span>
       </div>
       <div className="pl-8.5 flex flex-col gap-y-4">
         <span>{content}</span>
+        {fragment && type === "RESULT" && (
+          <FragmentCard
+            fragment={fragment}
+            isActiveFragment={isActiveFragment}
+            onFragmentClick={onFragmentClick}
+          />
+        )}
       </div>
     </div>
   );
--- src/modules/projects/ui/components/messages-container.tsx
+++ src/modules/projects/ui/components/messages-container.tsx
@@ -1,7 +1,7 @@
 import { useTRPC } from "@/trpc/client";
 import { useSuspenseQuery } from "@tanstack/react-query";
 import { MessageCard } from "./message-card";
-
+import { MessageForm } from "./message-from";
 
 interface Props {
   projectId: string;
@@ -14,23 +14,27 @@ export const MessagesContainer = ({ projectId }: Props) => {
       projectId,
     })
   );
-  return <div className="flex flex-col flex-1 min-h-0">
-    <div className="flex-1 min-h-0 overflow-y-auto">
-      <div className="pt-2 pr-1">
-        {messages.map((message) => (
-          <MessageCard 
-          key={message.id}
-          content={message.content}
-          role={message.role}
-          fragment={message.fragment}
-          createdAt={message.createdAt}
-          isActiveFragment={false}
-          onFragmentClick={() => {}}
-          type={message.type}
-          />
-        ))}
+  return (
+    <div className="flex flex-col flex-1 min-h-0">
+      <div className="flex-1 min-h-0 overflow-y-auto">
+        <div className="pt-2 pr-1">
+          {messages.map((message) => (
+            <MessageCard
+              key={message.id}
+              content={message.content}
+              role={message.role}
+              fragment={message.fragment}
+              createdAt={message.createdAt}
+              isActiveFragment={false}
+              onFragmentClick={() => {}}
+              type={message.type}
+            />
+          ))}
+        </div>
       </div>
-
+      <div className="relative p-3 pt-1">
+        <MessageForm projectId={projectId} />
+      </div>
     </div>
-  </div>;
+  );
 };
