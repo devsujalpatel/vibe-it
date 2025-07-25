e7095d065e8082536d9743b1696acc4c7af86af7
sujalpatelcoder <sujal7455@gmail.com>
2025-07-25 17:32:15 +0530

    messages ui created

--- src/modules/projects/ui/components/message-from.tsx
+++ src/modules/projects/ui/components/message-from.tsx
@@ -5,7 +5,7 @@ import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import TextareaAutosize from "react-textarea-autosize";
 import { ArrowUpIcon, Loader2Icon } from "lucide-react";
-import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 
 import { cn } from "@/lib/utils";
 import { useTRPC } from "@/trpc/client";
@@ -25,7 +25,7 @@ const formSchema = z.object({
 
 export const MessageForm = ({ projectId }: Props) => {
   const trpc = useTRPC();
-
+  const queryClient = useQueryClient();
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
@@ -33,7 +33,21 @@ export const MessageForm = ({ projectId }: Props) => {
     },
   });
 
-  const createmessage = useMutation(trpc.messages.create.mutationOptions());
+  const createmessage = useMutation(
+    trpc.messages.create.mutationOptions({
+      onSuccess: () => {
+        form.reset();
+        queryClient.invalidateQueries(
+          trpc.messages.getMany.queryOptions({ projectId })
+        );
+        // TODO: Invalidate usate status
+      },
+      onError: (error) => {
+        // TODO: Redirect to pricing page if specific error
+        toast.error(error.message);
+      },
+    })
+  );
 
   const onSubmit = async (values: z.infer<typeof formSchema>) => {
     await createmessage.mutateAsync({
