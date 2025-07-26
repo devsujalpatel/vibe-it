d306fdc6232a53ecf8f06d9905b3c30dba0d51d8
sujalpatelcoder <sujal7455@gmail.com>
2025-07-27 00:13:40 +0530

    file explorer created

--- src/components/fle-explorer.tsx
+++ src/components/fle-explorer.tsx
@@ -29,6 +29,63 @@ function getLanguageFromExtension(filename: string): string {
   return extension || "text";
 }
 
+interface FileBreadcrumbProps {
+  filePath: string;
+}
+
+const FileBreadcrumb = ({ filePath }: FileBreadcrumbProps) => {
+  const pathSegments = filePath.split("/");
+  const maxSegments = 4;
+  const renderBreadCrumbItems = () => {
+    if (pathSegments.length <= maxSegments) {
+      // Show all segments if 4 or less
+      return pathSegments.map((segment, index) => {
+        const isLast = index === pathSegments.length - 1;
+
+        return (
+          <Fragment key={index}>
+            <BreadcrumbItem>
+              {isLast ? (
+                <BreadcrumbPage className="font-medium">
+                  {segment}
+                </BreadcrumbPage>
+              ) : (
+                <span className="text-muted-foreground">{segment}</span>
+              )}
+            </BreadcrumbItem>
+            {!isLast && <BreadcrumbSeparator />}
+          </Fragment>
+        );
+      });
+    } else {
+      const firstSegment = pathSegments[0];
+      const lastSegment = pathSegments[pathSegments.length - 1];
+      return (
+        <>
+          <BreadcrumbItem>
+            <span className="text-muted-foreground">{firstSegment}</span>
+            <BreadcrumbSeparator />
+            <BreadcrumbItem>
+              <BreadcrumbEllipsis />
+            </BreadcrumbItem>
+            <BreadcrumbSeparator />
+            <BreadcrumbItem>
+              <BreadcrumbPage className="font-medium">
+                {lastSegment}
+              </BreadcrumbPage>
+            </BreadcrumbItem>
+          </BreadcrumbItem>
+        </>
+      );
+    }
+  };
+  return (
+    <Breadcrumb>
+      <BreadcrumbList>{renderBreadCrumbItems()}</BreadcrumbList>
+    </Breadcrumb>
+  );
+};
+
 interface FileExplorerProps {
   files: FileCollection;
 }
@@ -38,6 +95,8 @@ export const FileExplorer = ({ files }: FileExplorerProps) => {
     const fileKeys = Object.keys(files);
     return fileKeys.length > 0 ? fileKeys[0] : null;
   });
+
+  const [copied, setCopied] = useState(false);
 
   const treeData = useMemo(() => {
     return converFilesToTreeItem(files);
@@ -51,6 +110,16 @@ export const FileExplorer = ({ files }: FileExplorerProps) => {
     },
     [files]
   );
+
+  const handleCopy = useCallback(() => {
+    if (selectedFile && files[selectedFile]) {
+      navigator.clipboard.writeText(files[selectedFile]);
+      setCopied(true);
+      setTimeout(() => {
+        setCopied(false);
+      }, 2000);
+    }
+  }, [selectedFile, files]);
 
   return (
     <ResizablePanelGroup direction="horizontal">
@@ -66,17 +135,17 @@ export const FileExplorer = ({ files }: FileExplorerProps) => {
         {selectedFile && files[selectedFile] ? (
           <div className="h-full w-full flex flex-col">
             <div className="border-b bg-sidebar px-4 py-2 flex justify-between items-center gap-x-2">
-              {/* TODO: Breadcrumb */}
+              <FileBreadcrumb filePath={selectedFile} />
 
               <Hint text="Copy to clipboard" side="bottom">
                 <Button
                   variant={"outline"}
                   size={"icon"}
                   className="ml-auto"
-                  onClick={() => {}}
-                  disabled={false}
+                  onClick={handleCopy}
+                  disabled={copied}
                 >
-                  <CopyIcon />
+                  {copied ? <CopyCheckIcon /> : <CopyIcon />}
                 </Button>
               </Hint>
             </div>
